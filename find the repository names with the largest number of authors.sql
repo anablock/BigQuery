be41cb526f0e3d4af0e3f56269725fda34b0ad15
Vuk Djukic <vuchina@icloud.com>
2019-07-02 09:01:17 -0700

    Update find the repository names with the largest number of authors.sql

--- find the repository names with the largest number of authors.sql
+++ find the repository names with the largest number of authors.sql
@@ -1,6 +1,7 @@
  #standardSQL 
  SELECT COUNT(DISTINCT author.email) 
-      AS num_authors, REGEXP_EXTRACT(repo_name[ORDINAL(1)], r"([^/]+)$") AS repo 
+      AS num_authors, REGEXP_EXTRACT(repo_name[ORDINAL(1)], r"([^/]+)$") 
+      AS repo 
       FROM `bigquery-public-data.github_repos.commits` 
       GROUP BY repo 
       ORDER BY num_authors 
